277b63399227ddc77812373417a4caa84fe7e1d4
vikashsaini9789 <134952679+vikashsaini9789@users.noreply.github.com>
2023-06-13 11:40:06 +0530

    Update __init__.py

--- infinix/__init__.py
+++ infinix/__init__.py
@@ -2,7 +2,7 @@ from telethon import TelegramClient
 import logging 
 import time 
 
-openai_key = "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"
+
 
 api_id = "1125689"
 api_hash = "4772d1792ed194020a8fb06a91ffb8fa"
